07f9c667048f697cf4ce97acab89465293bdbf5f
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2021-01-05 13:34:05 -0800

    update and simplify background page, remove custom and art showing sections

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,23 +19,20 @@ export default class About extends Component {
                 <br></br>
                 <p>< GiBeveledStar /></p>
                 <br></br> */}
-                <p className="description">ART SHOWINGS</p>
-                <p className="description"><a href="http://www.boiseartmuseum.org/exhibition/higher-ground-2019/">Boise Art Museum, Higher Ground, 2019</a></p>
-                <br></br>
-                <p className="description">CUSTOM PIECES</p>
+                {/* <p className="description">CUSTOM PIECES</p>
                 <p className="description">Kokua</p>
                 <p className="description">Reef</p>
                 <p className="description">Toby</p>
                 <p className="description">Scruffy</p>
-                <p className="description">Kid Cudi</p>
-                <br></br>
-                <p className="description">ABOUT</p>
+                <br></br> */}
+                {/* <p className="description">ABOUT</p> */}
                 <p className="description">
-                    Anne is a young, up and coming artist in Tucson, Arizona. She specializes in oil paintings and drawings inspired by photographs, music and her imagination.
+                    Anne is a young, up and coming artist in Tucson, Arizona.
+                    She specializes in oil paintings and drawings inspired by photographs, music and her imagination.
                     Her artistic style is impressionistic and expressionistic.
                     She often emphasizes certain parts of the piece to highlight their importance or for aesthetics.
-                    Her awe-provoking style is realistic, abstract or bridges the two.
-                    When she's not painting or drawing, she may be running or playing the saxaphone.</p>
+                    Her style is realistic, abstract or bridges the two.
+                    When she's not painting or drawing, she may be running or playing the saxophone.</p>
                 <br></br>
                 <p className="description">< GiBeveledStar /></p>
             </div>
